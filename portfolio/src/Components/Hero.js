aa97d4daee0d2b142bc950b34a1e64906bc74a28
justin <justinpolacek@gmail.com>
2020-09-23 21:52:57 -0700

    finished

--- portfolio/src/Components/Hero.js
+++ portfolio/src/Components/Hero.js
@@ -4,7 +4,7 @@ class Hero extends Component{
     render(){
         return(
             <section className="hero">
-            <h2>G R E Y S C A L E</h2>
+            <h2>G R A Y S C A L E</h2>
             <p>A free, responsive, one page Bootstrap theme<br/> created by Start Bootstrap.</p>
               <button class="button">Get Started</button>
             
